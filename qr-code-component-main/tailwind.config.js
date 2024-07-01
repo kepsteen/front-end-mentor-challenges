f2a602054456ab266f34f16190ef08d8d3eedfd4
Cody Epstein <codyepstein@gmail.com>
2024-07-01 18:54:08 +0000

    Added custom color classes to tailwind

--- qr-code-component-main/tailwind.config.js
+++ qr-code-component-main/tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {colors: {
+        'light-grey-blue': '#D5E1EF',
+        'grey-blue': '#7D889E',
+        'dark-navy': '#1F314F',
+        'blue-shade': '#3685FF',
+        'blue': '#2C7DFA',
+      },},
   },
   plugins: [],
 }
